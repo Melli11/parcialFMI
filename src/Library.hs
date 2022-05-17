a3179abfae1c600077232b993920cff1e42ccd13
Melli11 <mingerez@gmail.com>
2022-05-16 23:58:49 -0300

    Punto 4 ok

--- src/Library.hs
+++ src/Library.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Eta reduce" #-}
 module Library where
 import PdePreludat
 import GHC.Generics (Par1(unPar1))
@@ -27,10 +29,10 @@ prestarleNMillones cantidad  =  cambiarLaDeuda (cantidad*1.5)
 -- se reduzca la cantidad de activos en el sector público y además que el ingreso
 -- per cápita disminuya en 20% si los puestos de trabajo son más de 100 ó 15% en
 -- -- -- caso contrario
--- reducirSectorPublicoEn :: Number -> Pais -> Pais
--- reducirSectorPublicoEn cantidad unPais
---     | sectorPublico unPais > 100 = cambiarSectorPublico ( negate cantidad ) .  cambiarIngresoPerCapital (negate 0.2 * (ingresoPerCapita unPais) )
---     | otherwise =  cambiarSectorPublico ( negate cantidad) .  cambiarIngresoPerCapital(negate 0.15 * ingresoPerCapita unPais) 
+reducirSectorPublicoEn :: Number -> Pais -> Pais
+reducirSectorPublicoEn cantidad unPais
+    | sectorPublico unPais > 100 = cambiarSectorPublico (negate cantidad) $ cambiarIngresoPerCapital (negate (0.2 * ingresoPerCapita unPais)) unPais
+    | otherwise =  cambiarSectorPublico (negate cantidad) $ cambiarIngresoPerCapital(negate (0.15 * ingresoPerCapita unPais)) unPais 
 
 darEmpresaAlFmi :: RecursoNatural -> Receta
 darEmpresaAlFmi recursoNatural  = explotarRecurso recursoNatural .  cambiarLaDeuda (negate 20000000)
@@ -46,9 +48,22 @@ establecerBlindaje unPais = prestarleNMillones (productoBrutoInterno unPais) $ c
 productoBrutoInterno :: Pais -> Number
 productoBrutoInterno unPais = ingresoPerCapita unPais + sectorPrivado unPais + sectorPrivado unPais
 
+-- Dar un ejemplo de cómo generar al país Namibia, cuyo ingreso per cápita
+-- es de 4140 u$s, la población activa del sector público es de 400.000, la
+-- población activa del sector privado es de 650.000, su riqueza es la minería
+-- y el ecoturismo y le debe 50 (millones de u$s) al FMI.
 
+namibia :: Pais
+namibia = Pais 4140 400 650000 [Mineria,Ecoturismo] 50000000
 
+bolivia :: Pais
+bolivia = Pais 4140 400 650000 [Petroleo,Ecoturismo] 40000000
 
+venezuela :: Pais
+venezuela = Pais 4140 400 650000 [Petroleo,Ecoturismo] 30000000
+
+brasil :: Pais
+brasil = Pais 4140 400 650000 [Ganaderia,Ecoturismo] 20000000
 
 
 -- Funciones Auxiliares
@@ -69,4 +84,25 @@ cambiarIngresoPerCapital cantidad unPais = unPais {ingresoPerCapita = ingresoPer
 
 -- Paises Auxiliares
 argentina :: Pais
-argentina = Pais 5 10 10 [Mineria, Petroleo] 1500000
+argentina = Pais 5 10 10 [Mineria, Petroleo] 10000000
+
+listaDePaises = [brasil,venezuela,bolivia,argentina,namibia]
+-- a.Dada una lista de países conocer cuáles son los que pueden zafar,
+-- aquellos que tienen "Petróleo" entre sus riquezas naturales.
+
+puedenZafar :: [Pais] -> [Pais]
+puedenZafar paises = filter (\pais  -> elem Petroleo $ recursos pais ) paises
+
+
+-- b. Dada una lista de países, saber el total de deuda que el FMI tiene a su
+-- favor.
+
+totalDeDeuda  = sum.map (deuda) 
+-- c. Indicar en dónde apareció cada uno de los conceptos (solo una vez) y
+-- justificar qué ventaja tuvo para resolver el requerimiento.
+
+-- Debe resolver este punto con recursividad: dado un país y una lista de
+-- recetas, saber si la lista de recetas está ordenada de “peor” a “mejor”, en base al
+-- siguiente criterio: si aplicamos una a una cada receta, el PBI del país va de menor
+-- a mayor. Recordamos que el Producto Bruto Interno surge de multiplicar el
+-- ingreso per cápita por la población activa (privada y pública).
